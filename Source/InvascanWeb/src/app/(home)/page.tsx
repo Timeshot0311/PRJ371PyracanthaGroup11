a06067e7ac2eea809d9f0a8c30b71e89130eb658
Ryan Thurbon <ryanthurbon3@gmail.com>
2025-05-10 16:41:26 +0200

    Setup basic web page with API integration
    I had to make a slight change to the docker file as i was running into issues. The requirements.txt was also giving me issues as some requirements listed did not exist with the specified version number - i made changes to fix this

---
+++ Source/InvascanWeb/src/app/(home)/page.tsx
@@ -0,0 +1,38 @@
+import ImageUploader from "@/components/image-uploader";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { ChartColumn } from "lucide-react";
+import Link from "next/link";
+
+export default function HomeIndex() {
+    return (
+        <div>
+            <section className='mx-auto container py-10 max-w-3xl px-4'>
+                <header className='flex items-center justify-between'>
+                    <Link href='/' className='text-xl font-medium'>
+                        Invascan
+                    </Link>
+                    <Button disabled>
+                        <ChartColumn className='mr-2 h-4 w-4' />
+                        Analytics (Coming soon)
+                    </Button>
+                </header>
+            </section>
+
+            <section className='max-w-3xl mx-auto container px-4 pb-10'>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Upload an image</CardTitle>
+                        <CardDescription>
+                            We will analyze your image to identify if it contains Pyracantha.
+                        </CardDescription>
+                    </CardHeader>
+
+                    <CardContent className='grid gap-6 md:grid-cols-2'>
+                        <ImageUploader />
+                    </CardContent>
+                </Card>
+            </section>
+        </div>
+    );
+}
