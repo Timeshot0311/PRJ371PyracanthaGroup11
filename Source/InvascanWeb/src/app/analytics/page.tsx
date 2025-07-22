29e1cd92fcb1833c92faa6b204373233cb604d83
Ryan Thurbon <ryanthurbon3@gmail.com>
2025-07-22 15:37:50 +0200

    nothing

--- Source/InvascanWeb/src/app/analytics/page.tsx
+++ Source/InvascanWeb/src/app/analytics/page.tsx
@@ -13,82 +13,92 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function HomeIndex() {
-  const [focusPoint, setFocusPoint] = useState<{ lng: number; lat: number } | null>(null);
-  const [chartData, setChartData] = useState<Observation[]>([]);
+    const [focusPoint, setFocusPoint] = useState<{ lng: number; lat: number } | null>(null);
+    const [chartData, setChartData] = useState<Observation[]>([]);
 
-  // Fetch iNaturalist data on component mount
-  useEffect(() => {
-    async function loadObservations() {
-      const data = await fetchObservations(54053, 6986); // Taxon ID and Place ID
-      setChartData(data); // Use the chartData state directly
-    }
-    loadObservations();
-  }, []);
+    // Fetch iNaturalist data on component mount
+    useEffect(() => {
+        async function loadObservations() {
+            const data = await fetchObservations(54053, 6986); // Taxon ID and Place ID
+            setChartData(data); // Use the chartData state directly
+        }
+        loadObservations();
+    }, []);
 
-  // Handle chart bar click to update map view
-  const handlePointClick = (lng: number, lat: number) => {
-    setFocusPoint({ lng, lat });
-  };
+    // Handle chart bar click to update map view
+    const handlePointClick = (lng: number, lat: number) => {
+        setFocusPoint({ lng, lat });
+    };
 
-  // Handle location search to update map view
-  const handleLocationSearch = (lng: number, lat: number) => {
-    setFocusPoint({ lng, lat });
-  };
+    // Handle location search to update map view
+    const handleLocationSearch = (lng: number, lat: number) => {
+        setFocusPoint({ lng, lat });
+    };
 
-  return (
-    <div>
-      <section className='mx-auto container py-10 max-w-3xl px-4'>
-        <header className='flex items-center justify-between'>
-          <Link href='/' className='text-xl font-medium'>
-            Invascan
-          </Link>
-          <Button variant='secondary' asChild>
-            <Link href='/'>
-              <UploadIcon className='mr-2 h-4 w-4' />
-              Analyze image
-            </Link>
-          </Button>
-        </header>
-      </section>
+    return (
+        <div>
+            <section className='mx-auto container py-10 max-w-3xl px-4'>
+                <header className='flex items-center justify-between'>
+                    <Link href='/' className='text-xl font-medium'>
+                        Invascan
+                    </Link>
+                    <Button variant='secondary' asChild>
+                        <Link href='/'>
+                            <UploadIcon className='mr-2 h-4 w-4' />
+                            Analyze image
+                        </Link>
+                    </Button>
+                </header>
+            </section>
 
-      <section className='max-w-3xl mx-auto container px-4 pb-10'>
-        <Card className='border-none shadow-none'>
-          <CardHeader>
-            <CardTitle>Pyracantha Detection Analytics</CardTitle>
-            <CardDescription>
-              Visualize geographic distribution and analytics for Pyracantha
-            </CardDescription>
-          </CardHeader>
-          <CardContent className='flex flex-col gap-6 w-full'>
-            <div className='flex flex-row gap-2'>
-              <LocationSearch onSearch={handleLocationSearch} />
-            </div>
+            <section className='max-w-3xl mx-auto container px-4 pb-10'>
+                <Card className='border-none shadow-none'>
+                    <CardHeader>
+                        <CardTitle>Pyracantha Detection Analytics</CardTitle>
+                        <CardDescription>
+                            Visualize geographic distribution and analytics for Pyracantha
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className='flex flex-col gap-6 w-full'>
+                        <div className='flex flex-row gap-2'>
+                            <LocationSearch onSearch={handleLocationSearch} />
+                        </div>
 
-            <div>
-              <Card className='min-h-[400px] p-0 border-none shadow-none'>
-                <CardContent className='h-[500px] px-0'>
-                  <div className='relative w-full h-full'>
-                    <InteractiveMap points={chartData} focusPoint={focusPoint} />
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
+                        <div>
+                            <Card className='min-h-[400px] p-0 border-none shadow-none'>
+                                <CardContent className='h-[500px] px-0'>
+                                    <div className='relative w-full h-full'>
+                                        <InteractiveMap
+                                            points={chartData}
+                                            focusPoint={focusPoint}
+                                        />
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </div>
 
-            <Tabs defaultValue='observation-statistics' className='w-full'>
-              <TabsList className='bg-transparent w-full'>
-                <TabsTrigger value='observation-statistics'>Observation Statistics</TabsTrigger>
-                <TabsTrigger value='distribution-graph'>Distribution Graph</TabsTrigger>
-              </TabsList>
-              <TabsContent value='observation-statistics' className='mt-10'>
-                <ObservationStatsChart chartData={chartData} />
-              </TabsContent>
-              <TabsContent value='distribution-graph' className='mt-10'>
-                <ObservationChart data={chartData} onPointClick={handlePointClick} />
-              </TabsContent>
-            </Tabs>
-          </CardContent>
-        </Card>
-      </section>
-    </div>
-  );
+                        <Tabs defaultValue='observation-statistics' className='w-full'>
+                            <TabsList className='bg-transparent w-full'>
+                                <TabsTrigger value='observation-statistics'>
+                                    Observation Statistics
+                                </TabsTrigger>
+                                <TabsTrigger value='distribution-graph'>
+                                    Distribution Graph
+                                </TabsTrigger>
+                            </TabsList>
+                            <TabsContent value='observation-statistics' className='mt-10'>
+                                <ObservationStatsChart chartData={chartData} />
+                            </TabsContent>
+                            <TabsContent value='distribution-graph' className='mt-10'>
+                                <ObservationChart
+                                    data={chartData}
+                                    onPointClick={handlePointClick}
+                                />
+                            </TabsContent>
+                        </Tabs>
+                    </CardContent>
+                </Card>
+            </section>
+        </div>
+    );
 }
--- Source/InvascanWeb/src/components/image-uploader.tsx
+++ Source/InvascanWeb/src/components/image-uploader.tsx
@@ -5,149 +5,148 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { AlertCircle } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export default function ImageUploader() {
-  const [imageUrl, setImageUrl] = useState<string | undefined>();
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [loading, setLoading] = useState(false);
-  const [label, setLabel] = useState<string>("");
-  const [score, setScore] = useState<number>(0);
-  const [isAnalyzed, setIsAnalyzed] = useState(false);
+    const [imageUrl, setImageUrl] = useState<string | undefined>();
+    const [file, setFile] = useState<File | undefined>(undefined);
+    const [loading, setLoading] = useState(false);
+    const [label, setLabel] = useState<string>("");
+    const [score, setScore] = useState<number>(0);
+    const [isAnalyzed, setIsAnalyzed] = useState(false);
 
-  const maxFileSize = 5 * 1024 * 1024; // 5MB
+    const maxFileSize = 5 * 1024 * 1024; // 5MB
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const selectedFile = e.target.files[0];
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            const selectedFile = e.target.files[0];
 
-      if (selectedFile.size > maxFileSize) {
-        toast.error("File too large. Maximum size is 5MB.");
-        return;
-      }
+            if (selectedFile.size > maxFileSize) {
+                toast.error("File too large. Maximum size is 5MB.");
+                return;
+            }
 
-      setFile(selectedFile);
-      setImageUrl(URL.createObjectURL(selectedFile));
-      setIsAnalyzed(false);
-    }
-  };
-
-  const convertToBase64 = (file: File): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        if (typeof reader.result === "string") {
-          const base64String = reader.result.split(",")[1];
-          resolve(base64String);
-        } else {
-          reject(new Error("Failed to convert image to base64"));
+            setFile(selectedFile);
+            setImageUrl(URL.createObjectURL(selectedFile));
+            setIsAnalyzed(false);
         }
-      };
-      reader.onerror = (error) => reject(error);
-    });
-  };
+    };
 
-  const submitImage = async () => {
-    if (!file) {
-      toast.error("No image selected.");
-      return;
-    }
+    const convertToBase64 = (file: File): Promise<string> => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => {
+                if (typeof reader.result === "string") {
+                    const base64String = reader.result.split(",")[1];
+                    resolve(base64String);
+                } else {
+                    reject(new Error("Failed to convert image to base64"));
+                }
+            };
+            reader.onerror = (error) => reject(error);
+        });
+    };
 
-    setLoading(true);
+    const submitImage = async () => {
+        if (!file) {
+            toast.error("No image selected.");
+            return;
+        }
 
-    try {
-      const base64Image = await convertToBase64(file);
+        setLoading(true);
 
-      await toast.promise(analyzeImage(base64Image), {
-        loading: "Analyzing image...",
-        success: (result) => {
-          if (!result.success || !result.imgUrl) {
-            throw new Error("API error occurred");
-          }
-          setImageUrl(result.imgUrl);
-          setLabel(result.label);
-          setScore(result.score);
-          setIsAnalyzed(true);
-          return result.message;
-        },
-        error: (error) => {
-          const errorMessage = error instanceof Error ? error.message : "Unknown error";
-          setIsAnalyzed(true); // Still mark as analyzed to show "Nothing detected" if appropriate
-          return errorMessage;
-        },
-        duration: 3000,
-        finally: () => setLoading(false), // Move setLoading(false) here
-      });
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "Unknown error";
-      toast.error(errorMessage);
-      setLoading(false); // Ensure loading is reset for errors outside the promise
-    }
-  };
+        try {
+            const base64Image = await convertToBase64(file);
 
-  return (
-    <>
-      <div className='flex flex-col justify-between gap-4 min-h-60'>
-        <label
-          htmlFor='file-upload'
-          className='flex-1 w-full flex p-4 border-dashed border rounded-lg text-muted-foreground items-center justify-center cursor-pointer'
-        >
-          <Input
-            id='file-upload'
-            type='file'
-            accept='image/jpeg'
-            className='sr-only'
-            onChange={handleImageChange}
-          />
-          <div className='flex flex-col gap-2 items-center text-center'>
-            <div>Upload Image</div>
-            <span className='text-xs'>JPEG (Max 5MB)</span>
-          </div>
-        </label>
-        <Button className='w-full' disabled={!file || loading} onClick={submitImage}>
-          {loading ? "Analyzing..." : "Analyze image"}
-        </Button>
-      </div>
+            toast.promise(analyzeImage(base64Image), {
+                loading: "Analyzing image...",
+                success: (result) => {
+                    if (!result.success || !result.imgUrl) {
+                        throw new Error("API error occurred");
+                    }
+                    setImageUrl(result.imgUrl);
+                    setLabel(result.label);
+                    setScore(result.score);
+                    setIsAnalyzed(true);
+                    return result.message;
+                },
+                error: (error) => {
+                    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+                    setIsAnalyzed(true); // Still mark as analyzed to show "Nothing detected" if appropriate
+                    return errorMessage;
+                },
+                duration: 3000,
+                finally: () => setLoading(false), // Move setLoading(false) here
+            });
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message : "Unknown error";
+            toast.error(errorMessage);
+            setLoading(false); // Ensure loading is reset for errors outside the promise
+        }
+    };
 
-      {imageUrl ? (
-        <div className='max-w-3xl w-full'>
-          <AspectRatio ratio={4 / 3} className='w-full overflow-hidden rounded-md'>
-            <Image
-              src={imageUrl}
-              alt='Image preview'
-              className='rounded-md object-cover'
-              fill
-              priority
-            />
-          </AspectRatio>
-        </div>
-      ) : (
-        <div className='flex justify-center items-center text-muted-foreground text-center h-48'>
-          <div>No image selected</div>
-        </div>
-      )}
+    return (
+        <>
+            <div className='flex flex-col justify-between gap-4 min-h-60'>
+                <label
+                    htmlFor='file-upload'
+                    className='flex-1 w-full flex p-4 border-dashed border rounded-lg text-muted-foreground items-center justify-center cursor-pointer'
+                >
+                    <Input
+                        id='file-upload'
+                        type='file'
+                        accept='image/jpeg'
+                        className='sr-only'
+                        onChange={handleImageChange}
+                    />
+                    <div className='flex flex-col gap-2 items-center text-center'>
+                        <div>Upload Image</div>
+                        <span className='text-xs'>JPEG (Max 5MB)</span>
+                    </div>
+                </label>
+                <Button className='w-full' disabled={!file || loading} onClick={submitImage}>
+                    {loading ? "Analyzing..." : "Analyze image"}
+                </Button>
+            </div>
 
-      {isAnalyzed && (
-        <div className='mt-4 text-lg col-span-2'>
-          {label && score > 0 ? (
-            <Alert variant='destructive'>
-              <AlertTitle className='text-lg'>Detected Pyracantha</AlertTitle>
-              <AlertDescription className='flex flex-col gap-1'>
-                <div>Type: {label}</div>
-                <div>Confidence: {score * 100}%</div>
-              </AlertDescription>
-            </Alert>
-          ) : (
-            <Alert>
-              <AlertTitle className='text-lg'>No pyracantha detected</AlertTitle>
-            </Alert>
-          )}
-        </div>
-      )}
-    </>
-  );
+            {imageUrl ? (
+                <div className='max-w-3xl w-full'>
+                    <AspectRatio ratio={4 / 3} className='w-full overflow-hidden rounded-md'>
+                        <Image
+                            src={imageUrl}
+                            alt='Image preview'
+                            className='rounded-md object-cover'
+                            fill
+                            priority
+                        />
+                    </AspectRatio>
+                </div>
+            ) : (
+                <div className='flex justify-center items-center text-muted-foreground text-center h-48'>
+                    <div>No image selected</div>
+                </div>
+            )}
+
+            {isAnalyzed && (
+                <div className='mt-4 text-lg col-span-2'>
+                    {label && score > 0 ? (
+                        <Alert variant='destructive'>
+                            <AlertTitle className='text-lg'>Detected Pyracantha</AlertTitle>
+                            <AlertDescription className='flex flex-col gap-1'>
+                                <div>Type: {label}</div>
+                                <div>Confidence: {score * 100}%</div>
+                            </AlertDescription>
+                        </Alert>
+                    ) : (
+                        <Alert>
+                            <AlertTitle className='text-lg'>No pyracantha detected</AlertTitle>
+                        </Alert>
+                    )}
+                </div>
+            )}
+        </>
+    );
 }
