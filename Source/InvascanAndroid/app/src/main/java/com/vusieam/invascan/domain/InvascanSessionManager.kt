904ff45b768b46686f30f051def05e85e4eb2ad7
Vusi Malinga <vusi@rqtech.co.za>
2026-01-27 23:00:35 +0200

    sync project

--- Source/InvascanAndroid/app/src/main/java/com/vusieam/invascan/domain/InvascanSessionManager.kt
+++ Source/InvascanAndroid/app/src/main/java/com/vusieam/invascan/domain/InvascanSessionManager.kt
@@ -5,7 +5,7 @@ import com.auth0.android.jwt.JWT
 
 object InvascanSessionManager {
     private const val PREFS_NAME = "my_app_prefs"
-    private const val KEY_ACCESS_TOKEN = "access_token"
+    private const val KEY_ACCESS_TOKEN = "auth_token"
 
     fun saveAuthToken(context: Context, token: String) {
         val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -30,6 +30,18 @@ object InvascanSessionManager {
             return ""
         }
         val jwt = JWT(token)
+        return jwt.getClaim("sub").asString() ?: jwt.getClaim("sub").asString()
+    }
+
+    fun getUserIdFromJWTToken(context: Context): String? {
+        val token = fetchAuthToken(context)
+        if(token?.isNullOrEmpty() == true){
+            return ""
+        }
+        val jwt = JWT(token)
+        // Claim used by your backend
+        val claimName = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+        val userId = jwt.getClaim(claimName).asString()
         return jwt.getClaim("sub").asString() ?: jwt.getClaim("sub").asString()
     }
 
